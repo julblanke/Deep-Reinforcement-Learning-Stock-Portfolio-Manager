a057fe79f02885e710f6c37071074b3c3aeb6f53
julblanke <j_blan06@wwu.de>
2024-03-12 12:28:49 +0100

    feat: adjusted to single- and multi-agent structure; incorporated yaml config file rather than args

--- drlpm/drlpm_run.py
+++ drlpm/drlpm_run.py
@@ -1,16 +1,16 @@
 import sys
 import pathlib
+
 sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
 import typer
 import logging
 import torch as th
-from typing import List
 from drlpm.utils.logger import Logger
-from drlpm.base.dataloader import Dataloader
-from drlpm.envs.stock_env import StockTradingEnv
-from drlpm.base.train_and_eval import TrainAndEval
-from drlpm.agents.stable_baselines_models import StableBaselinesModels
+from drlpm.utils.config_reader import ConfigReader
+from drlpm.base.multi_agent_drlpm import MultiAgentDrlpm
+from drlpm.base.single_agent_drlpm import SingleAgentDrlpm
+from drlpm.base.data_processing.dataloader import Dataloader
 
 
 DEVICE = th.device("cuda" if th.cuda.is_available() else "cpu")
@@ -20,87 +20,40 @@ class DrlPortfolioManager:
     """Deep Reinforcement Learning Portfolio Manager."""
 
     @staticmethod
-    def run(model: str, stock_symbols: list, initial_balance: float, train_timesteps: int, period: str,
-            interval: str, update_data: bool) -> None:
+    def run(config_path: str = "./examples/multi_agent.yaml") -> None:
         """Run the portfolio manager.
 
         Args:
-            model (str): Reinforcement learning model to use
-            stock_symbols (list): List of stock as stock-symbols, e.g. 'AAPL'
-            initial_balance (float): Initial account balance
-            train_timesteps (int): Number of model time steps for training
-            period (str): Time period for data to take into account -- in yfinance terms -- e.g. '2y'
-            interval (str): Data points frequency -- in yfinance terms -- e.g. '1d'
-            update_data (bool): Whether to update stock data
+            config_path (str): Path to yaml config file
         """
         Logger.initialize_logger()
         logger = logging.getLogger()
+        config = ConfigReader.execute(config_path=config_path)
 
-        # mapping of model name to respective python class
-        model_name_pyclass_dict = {
-            "PPO": StableBaselinesModels,
-            "A2C": StableBaselinesModels,
-            "DDPG": StableBaselinesModels,
-            "SAC": StableBaselinesModels,
-            "TD3": StableBaselinesModels
-        }
-
-        # load data
-        data = (Dataloader(stock_symbols=stock_symbols,
-                           period=period,
-                           interval=interval,
-                           update_data=update_data)
+        data = (Dataloader(stock_symbols=config["stock_symbols"],
+                           period=config["period"],
+                           interval=config["interval"],
+                           update_data=config["update_data"])
                 .get_data())
         logger.info("Loading data finished.")
 
-        # create environment
-        env = StockTradingEnv(data=data,
-                              stock_symbols=stock_symbols,
-                              initial_balance=initial_balance)
-        logger.info("Created environment.")
-
-        # create model
-        model = model_name_pyclass_dict[model](model_name=model).get_model(env=env,
-                                                                           verbose=1,
-                                                                           device=DEVICE,
-                                                                           tensorboard_log="./logs")
-
-        # train and eval
-        TrainAndEval.train_and_eval(model=model,
-                                    stock_symbols=stock_symbols,
-                                    data=data,
-                                    train_timesteps=train_timesteps)
-
+        if config["drlpm_type"] == "single_agent":
+            SingleAgentDrlpm.run_single_agent(config=config, data=data, device=DEVICE)
+        elif config["drlpm_type"] == "multi_agent":
+            MultiAgentDrlpm.run_multi_agent(config=config, data=data)
+        else:
+            raise KeyError(f"{config['drlpm_type']} not known."
+                           f" Only 'single_agent' and 'multi_agent' known.")
         logger.info("Done!")
 
 
-def main(stock_symbols: List[str] = typer.Argument(..., help="Define stock symbols to be considered for portfolio."),
-         model: str = typer.Option(..., help="Choose reinforcement learning model. Currently supported: \n"
-                                             "PPO, A2C, DDPG, SAC, TD3"),
-         initial_balance: float = typer.Option(..., help="Initial account balance."),
-         train_timesteps: int = typer.Option(..., help="Number of model time steps for training."),
-         period: str = typer.Option(..., help="Define time period to take into account."),
-         interval: str = typer.Option(..., help="Define time interval of stock values."),
-         update_data: bool = typer.Option(..., help="Update stock data of given stocks.")
-         ) -> None:
+def main(config_path: str = typer.Option(..., help="Define path to yml config file.")) -> None:
     """Main func for typer.
 
     Args:
-        stock_symbols (list): List of stock as stock-symbols, e.g. 'AAPL'
-        model (str): Reinforcement learning model to use
-        initial_balance (float): Initial account balance
-        train_timesteps (int): Number of model time steps for training
-        period (str): Time period for data to take into account -- in yfinance terms -- e.g. '2y'
-        interval (str): Data points frequency -- in yfinance terms -- e.g. '1d'
-        update_data (bool): Whether to update stock data
+        config_path (str): Path to yaml config file
     """
-    DrlPortfolioManager.run(model=model,
-                            stock_symbols=stock_symbols,
-                            initial_balance=initial_balance,
-                            train_timesteps=train_timesteps,
-                            period=period,
-                            interval=interval,
-                            update_data=update_data)
+    DrlPortfolioManager.run(config_path=config_path)
 
 
 if __name__ == "__main__":
